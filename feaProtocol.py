bd854ae95b76134b7ce4ee369252886a3e30ab20
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-19 20:35:08 +0100

    Test now passes. (changed a few field names in the json format)

--- feaProtocol.py
+++ feaProtocol.py
@@ -1,10 +1,13 @@
 import json
 
-class feaProtocol:
-    def __init__(self):
-       pass
-
-    def loadFromFile(self, fileName):
-        json_file = json.loads(fileName)
-        for step in json_file:
-            print step
+class Protocol:
+    def __init__(self, fileName):
+        with open(fileName) as contents:
+            json_protocol = json.load(contents)
+            self.steps = [Step(step) for step in json_protocol]
+class Step:
+    def __init__(self, props):
+        self.record =  (props['record'] == 'true') or True
+        self.connect = (props['connect'] == 'true') or True
+        self.duration = props['duration'] or 10
+        self.graph = props['graph'] or 'all'
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,13 +1,13 @@
-import feaProtocol
+from feaProtocol import *
 class TestProtocol:
 
-    def setup(self):
-        self.protocol = feaProtocol()
+    # def setup(self):
+    #     self.protocol = Protocol()
 
     def test_load(self):
-       self.protocol.loadFromFile('protocols/record.json')
-       assert len(self.protocol.steps) == 1
-       step = self.protocol.steps[0]
+       protocol = Protocol('./protocols/record.json')
+       assert len(protocol.steps) == 1
+       step = protocol.steps[0]
        assert step.record == True
        assert step.connect == True
        # assert(step.graph == ) Unsure how to specify this for now
