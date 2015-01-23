efa860850120e4cf76f4386829399ea1bae755d7
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-23 22:04:57 +0100

    fixed previously unseen bug where the main window was being garbage collected. fixed this by storing in a variable

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from pyqtgraph.Qt import QtGui, QtCore
 import signal
+import sys
 from muselo import *
 from feaProtocol import Protocol, Step
 signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -25,18 +26,19 @@ class MainWindow(QtGui.QMainWindow):
         self.setGeometry(1000,1000,1000,1000)
         self.setWindowTitle('fealines')
         self.executeProtocol()
+        print "showing window"
         self.show()
 
     def executeProtocol(self):
         pcl = Protocol('tests/protocols/record.json')
+        self.central_widget = QtGui.QWidget()
+        self.setCentralWidget(self.central_widget)
         for step in pcl.steps:
-            central_widget = QtGui.QWidget()
-            self.setCentralWidget(central_widget)
-            central_widget.setLayout(step.renderWidget())
+            self.central_widget.setLayout(step.renderWidget())
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    MainWindow()
+    main = MainWindow()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
