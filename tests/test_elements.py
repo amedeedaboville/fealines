67d56f066877df5d049bdf24a3b82778db3515f6
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-23 11:24:54 +0100

    Added red tests for muselo observer pattern

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,12 +1,22 @@
 from enaml.qt.qt_widget import QtWidget
+import muselo
 from Elements import EEGPlot
 
 
 class TestElements:
 
-    def testEegPlot(self):
+    def testEegPlotSetup(self):
         plot = EEGPlot('all')
         assert isinstance(plot.getWidget(), QtWidget)
+
+    def testEegPlotUpdate(self):
+        plot = EEGPlot('all')
+        assert muselo.server.listeners is not None  # check we have listeners (maybe move to a separate test_muselo?)
+        assert plot in muselo.server.listeners['alpha1']  # check we are listening to specific messages
+
+        plot.update('alpha1', 80)
+        assert plot.data['alpha1'][-1] == 80
+        # TODO: test long term storage of the plot's data
 
     def testSignalDescriptor(self):
         assert EEGPlot.read_signal('all') is not None
