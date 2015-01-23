afcd65935c242f896f20fa354c68bb6073edae0b
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-23 21:08:32 +0100

    Unfinished. Made good progress on registering listeners for muselo on EEGPlot lines (in a fairly clean way)

--- Elements.py
+++ Elements.py
@@ -1,25 +1,52 @@
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
+from functools import partial
+import muselo
+
 class EEGPlot:
     def __init__(self, signals):
         self.pw = pg.PlotWidget()
         self.data = {}
         self.plots = {}
         lines = set()
+        bands = ['alpha', 'beta', 'delta', 'gamma', 'theta']
+
         for sig in signals.split(","):
-            lines = lines.union(self.read_signal(sig))
+            signal_name = self.read_signal(sig)
+            lines = lines.union(signal_name)
+
         for line in lines:
-            print line
             self.data[line] = np.random.normal(size=100)
             self.plots[line] = self.pw.plot(title=line, y=self.data[line], pen=(0, 255, 0, 100))
-        #TODO: register callbacks for updates on the lines we are plotting
 
+            if line == 'fea':
+                muselo.server.register_listener('/muse/elements/alpha_relative', self.receive_fea)
+            else:
+                band_name = signal_name[:-1]
+                if band_name in bands:
+                    muselo.server.register_listener('/muse/elements/%s_relative' % band_name, partial(self.receive_band, line=signal_name))
+                else:
+                    raise KeyError("Couldn't understand signal name %s and didn't register with server" % signal_name)
+        # self.lines = lines
 
-    def update(self, signal, new_dp):
-        self.data[signal] = np.roll(self.data[signal], -1)
-        self.data[signal][-1] = new_dp/100.
-        self.plots[signal].setData(self.data[signal])
+    def receive_band(self, line, args):
+        print line
+        signal_number = int(line[-1])
+        new_dp = args[signal_number]
+        self.update_dp(line, new_dp)
+
+    def receive_fea(self, args):
+        print "receiving fea dp "
+        left_alpha = args[1]
+        right_alpha = args[2]
+        new_dp = right_alpha - left_alpha
+        self.update_dp('fea', new_dp)
+
+    def update_dp(self, line, new_dp):
+        self.data[line] = np.roll(self.data[line], -1)
+        self.data[line][-1] = new_dp/100.
+        self.plots[line].setData(self.data[line])
 
     @classmethod
     def read_signal(cls, sig):
