9b67a39fde230c79b84e17237fa60a9fb8ed38d6
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-24 19:32:48 +0100

    Added a default protocol that shows FEA

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ class MainWindow(QtGui.QMainWindow):
         self.show()
 
     def executeProtocol(self):
-        pcl = Protocol('tests/protocols/record.json')
+        pcl = Protocol('./default_protocol.json')
         self.central_widget = QtGui.QWidget()
         self.setCentralWidget(self.central_widget)
         for step in pcl.steps:
