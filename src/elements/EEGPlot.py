9892e1c303fc447b9e3cb299bbe100a3d10e2477
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-24 23:59:57 +0100

    Split Elements.py into a proper EEGPlot and TimerWidget files

--- src/elements/EEGPlot.py
+++ src/elements/EEGPlot.py
@@ -2,7 +2,7 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 from functools import partial
-import muselo
+from .. import muselo
 
 
 class EEGPlot:
@@ -84,30 +84,3 @@ class EEGPlot:
                         signals[lr + '-' + band])
 
         return signals[sig]
-
-
-class TimerWidget(QtGui.QLabel):
-    def __init__(self, time, callback):
-        """
-        :param time:
-        The duration of the countdown timer, in seconds
-        :param callback:
-        The callback for when the timer finishes
-        """
-        super(TimerWidget, self).__init__()
-
-        self.setText("%d:%02d" % (time / 60., time % 60))
-
-        self.time_left = time
-        self.total_timer = QtCore.QTimer(self)
-        self.total_timer.timeout.connect(callback)
-        self.total_timer.start(time * 1000)
-
-        self.seconds_timer = QtCore.QTimer(self)
-        self.seconds_timer.timeout.connect(self.updateDisplay)
-        self.seconds_timer.start(1000)
-
-    def updateDisplay(self):
-        self.time_left -= 1
-        self.setText("%d:%02d" % (self.time_left / 60., self.time_left % 60))
-
