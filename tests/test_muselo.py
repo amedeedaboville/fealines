37ac23974d745fc0bb671ab48e62afea06a2f7b9
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-24 19:06:26 +0100

    fixed muselo observer pattern tests

--- tests/test_muselo.py
+++ tests/test_muselo.py
@@ -2,22 +2,19 @@ import muselo
 import mock
 
 
-class TestMuse:
+def test_listeners():
+    assert muselo.server.listeners is not None
+    plot = mock.Mock()
 
-    def testListeners(self):
-        assert muselo.server.listeners is not None
-        plot = mock.Mock()
+    path = '/muse/elements/eeg/alpha1'
+    muselo.server.register_listener(path, plot)
+    assert plot in muselo.server.listeners[path]
 
-        path = '/muse/elements/eeg/alpha1'
-        muselo.server.register_listener(path, plot)
-        assert plot in muselo.server.listeners[path]
+    args = [10, 20, 30, 40]
+    muselo.server.receive_signal(path, args)
+    plot.assert_called_once_with(path, args)
 
-        args = [10, 20, 30, 40]
-        muselo.server.eeg_callback(path, args)
-        plot.assert_called_once_with(path, args)
-
-        plot.reset_mock()
-        muselo.server.remove_listener(path, plot)
-        muselo.server.eeg_callback(path, args)
-        assert not plot.called
-
+    plot.reset_mock()
+    muselo.server.remove_listener(path, plot)
+    muselo.server.receive_signal(path, args)
+    assert not plot.called
