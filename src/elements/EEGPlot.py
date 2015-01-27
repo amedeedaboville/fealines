cb7dcd848bca3050cc73c879eec587479e6fe656
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-01-27 18:31:59 +0100

    Separated the data in eegplot into plotted and stored data, and added a stop method

--- src/elements/EEGPlot.py
+++ src/elements/EEGPlot.py
@@ -7,9 +7,11 @@ import muselo
 class EEGPlot:
     def __init__(self, signals):
         self.pw = pg.PlotWidget()
-        self.data = {}
+        self.data = {}# All of the data points ever received
+        self.plot_data = {}# The datapoints that are shown on the screen
         self.plots = {}
         self.lines = set()
+        self.listeners = []
 
         for sig in signals.split(","):
             signal_name = self.read_signal(sig)
@@ -18,25 +20,32 @@ class EEGPlot:
     def start(self):
         bands = ['alpha', 'beta', 'delta', 'gamma', 'theta']
         for line in self.lines:
-            self.data[line] = np.random.normal(size=100)
-            self.plots[line] = self.pw.plot(title=line, y=self.data[line], pen=(0, 255, 0, 100))
+            self.data[line] = []
+            self.plot_data[line] = np.empty(100)
+            self.plots[line] = self.pw.plot(title=line, y=self.plot_data[line], pen=(0, 255, 0, 100))
 
             #For now we register listeners on the paths of both muse-player versions: 3.4 and 3.6
             if line == 'fea':
-                muselo.server.register_listener('/muse/elements/alpha_relative', self.receive_fea)
-                muselo.server.register_listener('/muse/dsp/elements/alpha', self.receive_fea)
+                self.register_muse_listener('/muse/elements/alpha_relative', self.receive_fea)
+
+                self.register_muse_listener('/muse/dsp/elements/alpha', self.receive_fea)
             else:
                 band_name = line[:-1]
                 if band_name in bands:
-                    muselo.server.register_listener('/muse/elements/%s_relative' % band_name,
+                    self.register_muse_listener('/muse/elements/%s_relative' % band_name,
                                                     partial(self.receive_band, line))
-                    muselo.server.register_listener('/muse/dsp/elements/%s' % band_name,
+                    self.register_muse_listener('/muse/dsp/elements/%s' % band_name,
                                                     partial(self.receive_band, line))
                 else:
                     raise KeyError("Couldn't understand signal name %s and didn't register with server" % line)
 
+    def register_muse_listener(self, path, callback): #We use this function to record all of our listeners
+        self.listeners.append((path, callback))
+        muselo.server.register_listener(path, callback)
+
     def stop(self):
-        pass # TODO: remove listeners
+        for path,callback in self.listeners:
+            muselo.server.remove_listener(path, callback)
 
     def get_widget(self):
         return self.pw
@@ -54,14 +63,17 @@ class EEGPlot:
         self.update_dp('fea', new_dp)
 
     def update_dp(self, line, new_dp):
-        self.data[line] = np.roll(self.data[line], -1)
-        self.data[line][-1] = new_dp
-        self.plots[line].setData(self.data[line])
+        self.data[line].append(new_dp)
+
+        self.plot_data[line] = np.roll(self.plot_data[line], -1)
+        self.plot_data[line][-1] = new_dp
+        self.plots[line].setData(self.plot_data[line])
 
     def serialize(self):
-        plot_dict = {}
-        plot_dict['lines'] = self.lines
-        plot_dict['data'] = self.data
+        plot_dict = {
+            "lines": self.lines,
+            "data": self.data
+        }
         return plot_dict
 
     @classmethod
