a15704bb05df9fcc64a687626bdb341d2491267f
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-02-02 19:19:57 +0100

    Preliminarily functional. Had to change strategy after finding out images are layered on top of each other

--- src/elements/Horseshoe.py
+++ src/elements/Horseshoe.py
@@ -1,9 +1,10 @@
+import random
 from PyQt4.QtCore import pyqtSignal
 from pyqtgraph.Qt import QtCore, QtGui
 import muselo
 
 
-class HorseshoeWidget(QtGui.QFrame):
+class HorseshoeWidget(QtGui.QLabel):
 
     trigger = pyqtSignal()
 
@@ -11,23 +12,31 @@ class HorseshoeWidget(QtGui.QFrame):
         super(HorseshoeWidget, self).__init__()
         self.horseshoe = [0, 0, 0, 0]
         self.forehead = 0
-        self.layout = QtGui.QHBoxLayout()
-        self.labels = []
+        self.layout = QtGui.QGridLayout()
+        self.labels = [0, 0, 0, 0]
         self.colors = []
 
         self.trigger.connect(self.update_labels)
         self.load_pixmaps()
+        self.update_labels()
 
-        for idx in self.horseshoe:
-            new_label = QtGui.QLabel()
-            self.labels.append(new_label)
-            self.layout.addWidget(new_label)
-        self.setLayout(self.layout)
+        # self.labels = [ QtGui.QLabel() for idx in self.horseshoe]
+        # The row/columns are:
+        # 1 | fore  |2
+        # 0 |    |3
+        # self.layout.addWidget(self.labels[0], 1, 0)
+        # self.layout.addWidget(self.labels[1], 0, 0)
+        # self.layout.addWidget(self.labels[2], 0, 3)
+        # self.layout.addWidget(self.labels[3], 1, 3)
+        # self.setLayout(self.layout)
 
-        self.setStyleSheet("HorseshoeWidget{background-image:  url('./img/horseshoe/horseshoe.png');}")
+        self.setStyleSheet("""HorseshoeWidget {
+        border-image:  url('./img/horseshoe/horseshoe.png');
+        background-repeat: no-repeat; }""")
 
         # self.setGeometry(300, 300, 350, 100)
-        self.resize(300, 300)
+        self.setFixedSize(84, 84)
+
         self.show()
 
     def start(self):
@@ -64,14 +73,27 @@ class HorseshoeWidget(QtGui.QFrame):
     def update_labels(self):
         print "labels"
         for idx,number in enumerate(self.horseshoe):
-            if number == 1:
-                desc = 'good'
-                self.labels[idx].setPixmap(self.pixmaps[desc][idx])
-            elif number == 2:
-                desc = 'ok'
-                self.labels[idx].setPixmap(self.pixmaps[desc][idx])
-            else:
-                desc = 'bad'
-                # self.labels[idx].setPixmap(None)
-                self.labels[idx].setPixmap(self.pixmaps['good'][idx])
-            print idx, number, desc
+            desc = random.choice(['bad', 'ok', 'good'])
+            self.labels[idx] = self.pixmaps[desc][idx]
+            # if number == 1:
+            #     desc = 'good'
+            #     self.labels[idx] = self.pixmaps[desc][idx]
+            # elif number == 2:
+            #     desc = 'ok'
+            #     self.labels[idx] = self.pixmaps[desc][idx]
+            # else:
+            #     desc = 'bad'
+            #     self.labels[idx] = self.pixmaps['good'][idx]
+            print idx, desc
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setCompositionMode(QtGui.QPainter.CompositionMode_SourceAtop)
+        qp.setBackgroundMode(0)
+        qp.setBrush(QtGui.QColor(255, 255, 255, 200))
+        qp.drawRect(0, 0, 84, 84)
+        for img in self.labels:
+            qp.drawPixmap(0, 0, 84, 84, img)
+        qp.end()
