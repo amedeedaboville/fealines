722a30b352251edd7698df4b5f96b4139e3f807d
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-02-03 15:56:36 +0100

    cleaned up Step initialization

--- src/Protocol/Step.py
+++ src/Protocol/Step.py
@@ -6,37 +6,46 @@ from pyqtgraph import QtGui
 
 class Step(object):
     def __init__(self, props):
-        self.record = (props['record'] == 'true') or True
-        if 'duration' in props:
-            times = [int(x) for x in props['duration'].split(":")]
-            self.duration = times[0]*3600 + times[1]*60 + times[2]
-        else:
-            self.duration = 600
 
-        self.graph = props['graph'] if 'graph' in props else 'all'
-        self.name = props['name'] if 'name' in props else ''
+        self.parse_properties(props)
+        self.initUI()
 
         self.data_dict = {"name": self.name}
 
-        self.timer = TimerWidget(self.duration, self.endStep)
+    def parse_properties(self, props):
+        self.data_widgets = {}
+        self.record = ('record' in props and props['record'] == 'true')
+        self.name = props['name'] if 'name' in props else ''
 
+        self.graph = ('graph' in props and props['graph'] != 'false')
+        if self.graph:
+            self.plot = EEGPlot(props['graph'])
+            self.data_widgets['plot'] = self.plot
+
+        if 'duration' in props:
+            times = [int(x) for x in props['duration'].split(":")]
+            self.duration = times[0]*3600 + times[1]*60 + times[2] #TODO: breaks on less than 2 ':'s
+        else: self.duration = 600
+
+    def initUI(self):
         self.widget = QtGui.QWidget()
+        self.timer_widget = TimerWidget(self.duration, self.endStep)
         self.grid = QtGui.QHBoxLayout()
-        self.grid.addWidget(self.timer)
+        self.grid.addWidget(self.timer_widget)
 
-        if self.graph is not None:
-            self.plot = EEGPlot(self.graph)
+        if self.graph:
             self.grid.addWidget(self.plot.pw)
             self.horseshoe = HorseshoeWidget(self.plot.pw)
         else:
             self.horseshoe = HorseshoeWidget(self.widget)
         self.widget.setLayout(self.grid)
-        self.data_widgets = {'plot': self.plot} # TODO: Add other kinds of data widgets like checkboxes
+
 
     def startStep(self, callback):
-        self.timer.start()
+        self.timer_widget.start()
         self.horseshoe.start()
-        self.plot.start()
+        if self.graph:
+            self.plot.start()
         self.callback = callback
         return self.widget
 
