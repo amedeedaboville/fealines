e020fd0cc00e963a4da8cad2f19e6a2edd59835b
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-02-04 21:11:41 +0100

    Partially functional progress bars, need to use slots to update them now

--- src/Protocol/ConnectionStep.py
+++ src/Protocol/ConnectionStep.py
@@ -11,44 +11,41 @@ class ConnectionStep(Step):
     def __init__(self, props):
         super(ConnectionStep, self).__init__(props)
         # self.say_timer = QtCore.QTimer()
-        self.fwidget = FillWidget(self.endStep)
-        self.grid.addWidget(self.fwidget)
 
-class FillWidget(QtGui.QWidget):
-    """
-    A widget that
-    """
-    def __init__(self, callback):
-        super(FillWidget, self).__init__()
-        self.done = callback
-        self.layout = QtGui.QVBoxLayout()
+        self.time_to_finish = 10
+        self.f_widget = QtGui.QWidget()
+        self.f_layout = QtGui.QVBoxLayout()
         self.progress_bars = [QtGui.QProgressBar() for _ in range(4)]
         self.timers = [QtCore.QElapsedTimer() for _ in range(4)]
+
+        for timer in self.timers:
+            timer.invalidate()
         for bar in self.progress_bars:
             bar.setMinimum(0)
-            bar.setMaximum(2000)
-            self.layout.addWidget(bar)
+            bar.setMaximum(self.time_to_finish)
+            self.f_layout.addWidget(bar)
 
-        self.setLayout(self.layout)
+        self.f_widget.setLayout(self.f_layout)
+        self.grid.addWidget(self.f_widget)
 
         muselo.server.register_listener('/muse/elements/horseshoe', self.receive_horseshoe)
 
     def receive_horseshoe(self, path, args):
         good = 0
-        for conn, timer in zip(args, self.timers):
-            print conn
-            if conn == 1 : #good
+        for idx, (conn, timer) in enumerate(zip(args, self.timers)):
+            print conn, idx
+            if conn == 1 or idx == 3: #good
                 if not timer.isValid():
                     timer.start()
                 else:
-                    print "Timer is still good, elapsed: %d" % (timer.elapsed())
-                    if timer.elapsed() > 2000:
+                    print "Timer is still good, elapsed: %f" % (timer.elapsed() / 1e3)
+                    if timer.elapsed() / 1e3 > self.time_to_finish:
                         good += 1
             else:
                 timer.invalidate()
         for bar, timer in zip(self.progress_bars, self.timers):
             if timer.isValid():
-                bar.setValue(timer.elapsed())
+                bar.setValue(timer.elapsed() / 1e3)
             else:
                 bar.setValue(0)
         if good == 4:
