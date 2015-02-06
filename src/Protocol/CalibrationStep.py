d9a4c2781dc84f0a21e765c5579c109b3810e396
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-02-06 20:57:48 +0100

    Fixed issue #1

--- src/Protocol/CalibrationStep.py
+++ src/Protocol/CalibrationStep.py
@@ -1,6 +1,7 @@
 from Step import Step
 import random
 from os import system
+import subprocess
 from pyqtgraph.Qt import QtCore, QtGui
 
 
@@ -18,17 +19,25 @@ class CalibrationStep(Step):
         self.say_timer = QtCore.QTimer()
 
     def startStep(self, callback):
-        super(Step, self).startStep(callback)
+        self.callback = callback
+        self.timer_widget.start()
+        self.horseshoe.start()
+
         self.say_instructions()
-        self.say_timer.singleShot(1 * 1000, self.next_word)
+        self.say_timer.singleShot(1 * 7000, self.next_word)
+        return self.widget
+
+    def endStep(self):
+        super(Step, self).endStep()
+        self.thread.quit()
 
     def say_instructions(self):
         instructions = "When you hear each category, try to think of as many things that belong in that category"
-        system("say '%s'" % instructions)
+        self.say_string(instructions)
 
     def say_word(self):
         word = random.choice(self.words)
-        system("say '%s'" % word)
+        self.say_string(word)
 
     def next_word(self):
         if self.word_count < 5:
@@ -39,3 +48,6 @@ class CalibrationStep(Step):
             print "calibration almost over"
             self.say_timer.singleShot(5 * 1000, self.endStep)
 
+    def say_string(self, string):
+        subprocess.Popen(["say", string])
+        # system(u'say "{0:s}"'.format(string))
