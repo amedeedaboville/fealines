2153825895dd5e7861b6c3b49ac51e8f4cc613ab
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-02-07 12:09:56 +0100

    changes to setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 setup(
         name = "fealines",
         version = "0.1",
-        author = "Amédée d'Aboville",
+        author = "Amedee d'Aboville",
         author_email = "amedee.daboville@gmail.com",
         description = ("A PyQt application to visualize interaxon Muse data and perform biofeedback."),
         license = "BSD",
         keywords = "pyqt4 eeg muse",
         packages=['src', 'tests'],
         install_requires=[
-            'PyQt4',
-            'pyqtgraph'
+            'pyqtgraph',
+            'pyliblo'
             ],
         classifiers=[
             "Development Status :: 3 - Alpha",
