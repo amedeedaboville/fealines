5a1d4f9b9ff1b4a5b15193a4184f43a18a7e44bb
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-02-14 19:12:05 +0100

    Added a recent protocols menu

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import signal
-
+import json
+import os.path
 
 from PyQt4.QtGui import QMainWindow, QAction, QLabel, QApplication
+from PyQt4.QtCore import QVariant
 from muselo import *
 from Protocol.feaProtocol import Protocol, ProtocolNotLoaded
 
@@ -10,7 +12,38 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.open_config()
+        self.num_recent_pcl = 5
         self.initUI()
+
+    def open_config(self):
+        cfg_filename = 'config.json'
+        if os.path.exists(cfg_filename):
+            self.config = json.load(open(cfg_filename))
+        else:
+            self.config = {}
+        print self.config
+
+        recent_key = "recent_protocols"
+        self.config.setdefault(recent_key, [])
+        self.recent_pcl_filenames = self.config[recent_key]
+        self.recent_pcls = []
+        for pcl_filename in self.recent_pcl_filenames:
+            with open(pcl_filename) as pcl_file:
+                pcl = json.load(pcl_file)
+                pcl.setdefault("name", "Unnamed Protocol")
+                pcl.setdefault("description", "This protocol doesn't have a description.")
+                self.recent_pcls.append({
+                    "name": pcl['name'],
+                    "description": pcl['description'],
+                    "filename": pcl_filename
+                })
+
+    def open_recent_pcl(self):
+        action = self.sender()
+        if action:
+            filename = str(action.data().toString())
+            self.executeProtocol(filename)
 
     def initUI(self):
         closeAction = QAction('Close', self)
@@ -23,13 +56,22 @@ class MainWindow(QMainWindow):
         loadAction.triggered.connect(self.executeProtocol)
 
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu = menubar.addMenu('&Load')
-        fileMenu.addAction(closeAction)
+        file_menu = menubar.addMenu('&File')
+        file_menu.addAction(closeAction)
+        load_menu = menubar.addMenu('&Load')
+        load_menu.addAction(loadAction)
 
         self.toolbar = self.addToolBar('Exit')
         self.toolbar.addAction(closeAction)
         self.toolbar.addAction(loadAction)
+
+        load_menu.addSeparator()
+        for idx, recent_pcl in enumerate(self.recent_pcls):
+            print recent_pcl['name']
+            new_action = QAction("%d %s" % (idx + 1, recent_pcl['name']), self)
+            new_action.triggered.connect(self.open_recent_pcl)
+            new_action.setData(QVariant(recent_pcl['filename']))
+            load_menu.addAction(new_action)
 
         self.setGeometry(1000,1000,1000,1000)
         self.setWindowTitle('fealines')
@@ -37,10 +79,10 @@ class MainWindow(QMainWindow):
         self.show()
 
 
-    def executeProtocol(self):
-        print "Executing protocol"
+    def executeProtocol(self, filename="./protocols/graph_only.json"):
+        print u"loading protocol {0:s}".format(filename)
         try:
-            self.pcl = Protocol('./protocols/graph_only.json', self.protocolEnded)
+            self.pcl = Protocol(filename, self.protocolEnded)
         except ProtocolNotLoaded:
             print "protocol not loaded..."
         self.central_widget = self.pcl.main_widget
@@ -54,7 +96,6 @@ class MainWindow(QMainWindow):
 def main():
     app = QApplication(sys.argv)
     main = MainWindow()
-    main.executeProtocol()
     app.exec_()
     sys.exit(0)
 
